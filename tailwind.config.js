7f749fa81c3fdbe4eff03ab4bb784b3fcce23bc9
Nicholas Graham <nsgraham98@gmail.com>
2025-04-07 12:00:34 -0600

    Added starting file structure

---
+++ tailwind.config.js
@@ -0,0 +1,11 @@
+    /* Written by copilot */
+    /* just created as an outline/starting point */
+    /* Change anything as needed */
+
+module.exports = {
+    content: [],
+    theme: {
+        extend: {},
+    },
+    plugins: [],
+};
