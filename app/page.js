96c33ab8a6a281890c0921a36bc9997b0ebd53d3
annie <annemarie.ala@edu.sait.ca>
2025-04-08 14:46:51 -0600

    edit

--- app/page.js
+++ app/page.js
@@ -5,7 +5,13 @@ export default function Home() {
   return (
     <div>
       <h1 className="text-3xl font-bold underline">Home Page</h1>
+<<<<<<< Updated upstream
       <ExamplePage />
+=======
+      <Controls />
+      <SearchBar />
+      <Results />
+>>>>>>> Stashed changes
     </div>
   );
 }
