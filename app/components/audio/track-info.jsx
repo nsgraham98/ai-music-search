f0bafbcacb81903a4d62afd6b1718da8a9f33261
Nicholas Graham <nsgraham98@gmail.com>
2025-04-30 15:41:18 -0600

    added redirect links to track info + playlist items

--- app/components/audio/track-info.jsx
+++ app/components/audio/track-info.jsx
@@ -1,12 +1,28 @@
 "use client";
 
-import { Avatar, Box, Typography } from "@mui/material";
+import { Avatar, Box, Button, Typography } from "@mui/material";
 import { useAudioPlayerContext } from "@/context/audio-player-context";
+import {
+  goToArtist,
+  goToTrack,
+  goToAlbum,
+} from "@/app/api/jamendo/jamendo-handler/go-to-jamendo";
 
 export const TrackInfo = () => {
   const { currentTrack } = useAudioPlayerContext();
 
   if (!currentTrack) return null;
+
+  const handleTrackOnClick = (track) => {
+    goToTrack(track);
+  };
+
+  const handleArtistOnClick = (track) => {
+    goToArtist(track);
+  };
+  const handleAlbumOnClick = (track) => {
+    goToAlbum(track);
+  };
 
   return (
     <Box display="flex" alignItems="center" gap={2} sx={{ maxWidth: "100%" }}>
@@ -14,16 +30,49 @@ export const TrackInfo = () => {
       <Avatar
         src={currentTrack.image || ""}
         variant="rounded"
-        sx={{ width: 100, height: 100, bgcolor: "grey.500", flexShrink: 0 }}
+        sx={{
+          width: 100,
+          height: 100,
+          bgcolor: "grey.500",
+          flexShrink: 0,
+          cursor: "pointer",
+        }}
+        onClick={() => handleAlbumOnClick(currentTrack)}
       />
 
       {/* Text Info */}
       <Box sx={{ overflow: "hidden" }}>
-        <Typography variant="subtitle1" fontWeight="bold" color="white" noWrap>
+        <Typography
+          variant="subtitle1"
+          fontWeight="bold"
+          color="white"
+          noWrap
+          sx={{
+            cursor: "pointer",
+            color: "white",
+            transition: "color 0.1s",
+            "&:hover": {
+              color: "#E03FD8",
+            },
+          }}
+          onClick={() => handleTrackOnClick(currentTrack)}
+        >
           {currentTrack.name}
         </Typography>
-
-        <Typography variant="subtitle2" color="white" noWrap>
+        <Typography
+          variant="subtitle2"
+          color="white"
+          noWrap
+          sx={{
+            cursor: "pointer",
+            color: "white",
+            transition: "color 0.1s",
+            "&:hover": {
+              color: "#E03FD8",
+            },
+          }}
+          onClick={() => handleArtistOnClick(currentTrack)}
+        >
           {currentTrack.artist_name}
         </Typography>
       </Box>
