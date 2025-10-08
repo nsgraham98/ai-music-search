0793e57c573fc88b1084717fdfc43dc7afe50466
Nicholas Graham <nsgraham98@gmail.com>
2025-10-08 12:08:03 -0600

    adding some comments for reminders later

--- context/auth-context.jsx
+++ context/auth-context.jsx
@@ -128,16 +128,6 @@ export const AuthContextProvider = ({ children }) => {
 
   // Listener for auth state changes
   // Sets the user state (logged in user or null) and loading state (is mid login or not)
-  // useEffect(() => {
-  //   const unsubscribe = onAuthStateChanged(auth, async (currentUser) => {
-  //     setUser(currentUser);
-  //     setLoadingUser(false);
-  //   });
-  //   return () => unsubscribe();
-  // }, []);
-
-  // Listener for auth state changes
-  // Sets the user state (logged in user or null) and loading state (is mid login or not)
   useEffect(() => {
     const unsubscribe = onAuthStateChanged(auth, async (currentUser) => {
       if (authFlowComplete) {
@@ -147,29 +137,6 @@ export const AuthContextProvider = ({ children }) => {
     });
     return () => unsubscribe();
   }, [authFlowComplete]);
-
-  // Check for existing session on initial load
-  // TODO - write get session function in api route
-  // also consider moving this to a component that wraps the app, so it only runs once on app start
-  // useEffect(() => {
-  //   const checkSession = async () => {
-  //     try {
-  //       const response = await fetch("/api/auth/session", {
-  //         method: "GET",
-  //         credentials: "include",
-  //       });
-  //       if (response.ok) {
-  //         const sessionData = await response.json();
-  //         // TODO - set user based on session data
-  //       }
-  //     } catch (error) {
-  //       console.error("Error checking session:", error);
-  //     } finally {
-  //       setLoadingUser(false);
-  //     }
-  //   };
-  //   checkSession();
-  // }, []);
 
   return (
     <AuthContext.Provider
