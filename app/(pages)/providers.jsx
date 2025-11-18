23f07713f491448fa6bd6859b0923d56d20198eb
Nicholas Graham <nsgraham98@gmail.com>
2025-11-17 22:26:13 -0700

    more pre PR cleanup

--- app/(pages)/providers.jsx
+++ app/(pages)/providers.jsx
@@ -7,7 +7,6 @@ import { AudioPlayerProvider } from "@/context/audio-player-context.jsx";
 import { AuthContextProvider } from "@/context/auth-context.jsx";
 import { UserProfileContextProvider } from "@/context/user-profile-context.jsx";
 import ClientErrorBoundary from "@/app/components/error-boundary-client.jsx";
-import { TestingContextProvider } from "@/context/testing-context";
 import { AudioPlayer } from "@/app/components/audio/audio-player.jsx";
 import { SearchContextProvider } from "@/context/search-context";
 
@@ -17,10 +16,8 @@ export function Providers({ children }) {
       <AudioPlayerProvider>
         <AuthContextProvider>
           <UserProfileContextProvider>
-            <TestingContextProvider>
-              <AudioPlayer />
-              <SearchContextProvider>{children}</SearchContextProvider>
-            </TestingContextProvider>
+            <AudioPlayer />
+            <SearchContextProvider>{children}</SearchContextProvider>
           </UserProfileContextProvider>
         </AuthContextProvider>
       </AudioPlayerProvider>
